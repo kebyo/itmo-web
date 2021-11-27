1d2f7e25d97e9f5ec543b804733378d29020bd5a
Oybek <oybekmuslimov15@gmail.com>
2021-11-27 19:49:42 +0300

    Переделал подсчет времени загрузки страницы через timing

--- js/timer.js
+++ js/timer.js
@@ -1,15 +1,14 @@
 (function iife () {
-    const start = Date.now();
-
     window.addEventListener('load', () => {
-        const diff = Date.now() - start;
+        const timing = window.performance.timing;
+        const pageLoadTime = timing.loadEventStart - timing.navigationStart;
         const newElement = document.createElement('section');
 
         const main = document.querySelector('.header__avatar');
 
-        newElement.innerHTML = `<p>Страница была загружена за ${diff} миллисекунд</p>`
+        newElement.innerHTML = `<p>Страница была загружена за ${pageLoadTime} миллисекунд</p>`
         newElement.style.margin = '0';
 
         main.insertAdjacentElement('beforebegin', newElement);
-    })
+    });
 })();
