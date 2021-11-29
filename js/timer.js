481f3256de126ca07794c5f0bc598f37c51ac529
Oybek <oybekmuslimov15@gmail.com>
2021-11-29 14:03:29 +0300

    commit for deploy

--- js/timer.js
+++ js/timer.js
@@ -3,7 +3,6 @@
         const timing = window.performance.timing;
         const pageLoadTime = timing.loadEventStart - timing.navigationStart;
         const newElement = document.createElement('section');
-
         const main = document.querySelector('.header__avatar');
 
         newElement.innerHTML = `<p>Страница была загружена за ${pageLoadTime} миллисекунд</p>`
